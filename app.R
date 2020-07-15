68832c451a2f7ca24005bf950ea80c67fe6fb212
slfan2013 <slfan2013@users.noreply.github.com>
2020-07-15 14:39:23 -0700

    Update app.R

--- app.R
+++ app.R
@@ -665,7 +665,7 @@ server = shinyServer(function(input, output) {
     shinyjs::enable("downloadData")
     
     # RSD(e_norm[,p$sampleType=='validate'])
-    showNotification("Does this show? If shown, then error occurs before Aggregating Normalized Compounds...", duration = 15000)
+    # showNotification("Does this show? If shown, then error occurs before Aggregating Normalized Compounds...", duration = 15000)
     # return(paste0("The average QC cross-validated RSD changed from ",signif(median(qc_RSDs[[1]]*100),2),"% to ",signif(median(qc_RSDs[[2]]*100),2),"%. Now you can click Download Result button to save results."))
     return("TRUE")
     
